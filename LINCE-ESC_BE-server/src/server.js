72f9d56f412bd8a9f333d71103bb7f216fd7ac89
Rafael Baldessini Campanini <rafaelcampanini90@gmail.com>
2024-10-27 01:38:21 -0300

    "LINCE-ESC_BE-server" funcional!

--- LINCE-ESC_BE-server/src/server.js
+++ LINCE-ESC_BE-server/src/server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const fs = require('fs');
-const { console } = require('inspector');
 const path = require('path');
 
 const app = express();
@@ -9,20 +8,20 @@ const PORT = 3000;
 // Middleware para interpretar o corpo das requisições como JSON
 app.use(express.json());
 
-// Rotas para recebimento de dados (via POST)
+// Rota para receber dados (via POST)
 app.post('/dados', (req, res) => {
     const dados = req.body;
 
-    // Carrega  o arquivo existente ou cria umarray vazio
+    // Carrega o arquivo existente ou inicializa um array vazio
     let dadosSalvos = [];
-    const caminhoArquivo = path.joint(__dirname + 'dados.json');
+    const caminhoArquivo = path.join(__dirname, 'dados.json');
 
-    // Tenta ler o arquivo JSON para adicionar corretamente os novos dados
+    // Tenta ler o arquivo JSON para adicionar os novos dados
     try {
         const dadosExistentes = fs.readFileSync(caminhoArquivo, 'utf-8');
         dadosSalvos = JSON.parse(dadosExistentes);
     } catch (error) {
-        console.log("Arquivo NÃO encontrado. Criando um novo...");
+        console.log('Arquivo não encontrado. Criando um novo.');
     }
 
     // Adiciona os novos dados recebidos ao array
@@ -30,19 +29,19 @@ app.post('/dados', (req, res) => {
 
     // Salva o array atualizado no arquivo JSON
     fs.writeFileSync(caminhoArquivo, JSON.stringify(dadosSalvos, null, 2));
-
+    
     res.send('Dados recebidos e salvos com sucesso!');
 });
 
 // Rota para visualizar os dados
 app.get('/dados', (req, res) => {
-    const caminhoArquivo = path.join(__dirname + 'dados.json');
+    const caminhoArquivo = path.join(__dirname, 'dados.json');
 
     try {
         const dados = fs.readFileSync(caminhoArquivo, 'utf-8');
         res.send(JSON.parse(dados));
     } catch (error) {
-        res.send('Nenhum dado encontrado!');
+        res.send('Nenhum dado encontrado.');
     }
 });
 
